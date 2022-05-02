505f0c482301c4eb5300b726ebd06d3ccbe60c8d
Norbert Gocze <norbert.gocze@lensa.com>
2022-05-02 11:09:53 +0200

    Extend model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from fastapi import FastAPI
 
+from models import Item
+
 app = FastAPI()
 
 
@@ -12,3 +14,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
